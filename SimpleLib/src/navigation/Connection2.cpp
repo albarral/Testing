615c64046259f311bf24af1a108313fab0319aa9
albarral <albarral@migtron.com>
2015-10-05 22:22:21 +0200

    Some tests projects moved from sandbox repo.

---
+++ SimpleLib/src/navigation/Connection2.cpp
@@ -0,0 +1,16 @@
+/***************************************************************************
+ *   Copyright (C) 2015 by Migtron Robotics   *
+ *   albarral@migtron.com   *
+ ***************************************************************************/
+
+#include "Connection2.h"
+
+namespace sam 
+{
+Connection2::Connection2()
+{
+    place = -1;
+    nextPlace= -1;
+}
+
+}
